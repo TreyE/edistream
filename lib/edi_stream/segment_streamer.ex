c4d5a1c6232149267ceaaf790c609a01b18a2624
Trey Evans <lewis.r.evans@gmail.com>
2016-10-17 18:02:04 -0400

    Move to a stream for more control.

--- lib/edi_stream/segment_streamer.ex
+++ lib/edi_stream/segment_streamer.ex
@@ -1,48 +1,44 @@
 defmodule EdiStream.SegmentStreamer do
 
-  def stream_data(<<data::binary-size(1), rest::binary>>, f_sep, s_sep, fields, current_field, callback, counter, callback_state) do
-    case data do
-      ^f_sep -> stream_data(rest, f_sep, s_sep, [current_field|fields], "", callback, counter, callback_state)
-      ^s_sep ->
-        callback.(callback_state, clean_fields(Enum.reverse([current_field|fields])), counter)
-        stream_data(rest, f_sep, s_sep, [], "", callback, counter + 1, callback_state)
-      _ -> stream_data(rest, f_sep, s_sep, fields, current_field <> data, callback, counter, callback_state)
+  defp segment_streamer({f_sep, s_sep, fields, current_field, counter, io_thing}) do
+    case IO.binread(io_thing, 512) do
+      {:error, reason} -> raise reason
+      :eof -> {:halt, {f_sep, s_sep, fields, current_field, counter, io_thing}}
+      data -> 
+        {fs, cfs, new_counter, segments} = stream_segs(data, f_sep, s_sep, fields, current_field, counter, [])
+        {segments, {f_sep, s_sep, fs, cfs, new_counter, io_thing}}
     end
   end
 
-  def stream_data(<<>>, _f_sep, _s_sep, fields, current_field, _callback, counter, _callback_state) do
-    {fields, current_field, counter}
-  end
-
-  def stream_segments(io_thing, start_callback, segment_callback, finished_callback, callback_state \\ []) do
+  def segment_stream(io_thing) do
     sep_reader = EdiStream.DelimiterDetector.find_separators(io_thing)
     case sep_reader do
       {:ok, f_sep, s_sep} -> 
-        {:ok, init_state} = start_callback.(callback_state, f_sep, s_sep)
-        perform_streaming(io_thing, f_sep, s_sep, segment_callback, finished_callback, init_state)
+        acc = {}
+        {:ok, 
+           Stream.resource(fn -> {f_sep, s_sep, [], "", 0, io_thing} end,
+                           fn acc -> segment_streamer(acc) end,
+                           fn acc -> acc end)
+        }
       {:error, other} -> {:error, other}
     end
   end
 
-  defp clean_fields(fields) do
-    Enum.map(fields, &String.strip/1)
+  def stream_segs(<<data::binary-size(1), rest::binary>>, f_sep, s_sep, fields, current_field, counter, current_segments) do
+    case data do
+      ^f_sep -> stream_segs(rest, f_sep, s_sep, [current_field|fields], "", counter, current_segments)
+      ^s_sep ->
+        stream_segs(rest, f_sep, s_sep, [], "", counter + 1, [clean_fields(Enum.reverse([current_field|fields]))|current_segments])
+      _ -> stream_segs(rest, f_sep, s_sep, fields, current_field <> data, counter, current_segments)
+    end
   end
 
-  defp perform_streaming(io_thing, f_sep, s_sep, callback, finished_callback, callback_state) do
-    stream = IO.binstream(io_thing, 1024)
-    {leftover_fields, leftover_field, current_count} = Enum.reduce(stream, {[], "", 0}, fn(x, acc) -> 
-      {fs, cf, cnt} = acc
-      stream_data(x, f_sep, s_sep, fs, cf, callback, cnt, callback_state)
-    end)
-    leftover_f = String.strip(leftover_field)
-    leftover_fs = clean_fields(Enum.reverse(leftover_fields))
-    case {leftover_fs, leftover_f} do
-      {[], ""} -> 
-        finished_callback.(callback_state, leftover_fs, leftover_f, current_count)
-        {:ok, current_count}
-      _ -> 
-        finished_callback.(callback_state, leftover_fs, leftover_f, current_count)
-        {:warning, {:leftovers, leftover_fs, leftover_f}}
-    end
+  def stream_segs(<<>>, _f_sep, _s_sep, fields, current_field, counter, current_segs) do
+    {fields, current_field, counter, Enum.reverse(current_segs)}
+  end
+
+
+  defp clean_fields(fields) do
+    Enum.map(fields, &String.strip/1)
   end
 end
--- test/edi_stream/segment_streamer_test.exs
+++ test/edi_stream/segment_streamer_test.exs
@@ -2,12 +2,10 @@ defmodule EdiStream.SegmentStreamerTest do
   use ExUnit.Case
   doctest EdiStream.SegmentStreamer
 
-  test "the truth" do
-    start_callback = fn(cs, f_s, s_s) -> IO.puts("Starting parse: #{inspect f_s} : #{inspect s_s}"); {:ok, []} end
-    callback = fn(cs, data, counter) -> IO.puts("#{inspect counter} : #{inspect data}") end
-    leftover_callback = fn(cs, lfs, lf, counter) -> IO.puts("leftover: #{inspect lfs}, #{inspect lf}, #{inspect counter}") end
-    error_callback = fn(cs, err) -> IO.puts("error: #{inspect err}") end
+  test "parse as stream" do
     {:ok, f} = :file.open("b2b_edi.csv", [:read, :binary])
-    EdiStream.SegmentStreamer.stream_segments(f, start_callback, callback, leftover_callback, error_callback)
+    {:ok, stream} = EdiStream.SegmentStreamer.segment_stream(f)
+    Enum.each(stream,
+      fn x -> IO.puts("Data: #{inspect x}") end)
   end
 end
